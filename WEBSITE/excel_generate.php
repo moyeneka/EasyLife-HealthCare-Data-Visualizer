f4ca0cc5d4b47db1c4046d6ce1c1988e8fe22578
Micheal Oyenekan <60825726+moyeneka@users.noreply.github.com>
2021-11-20 11:21:59 -0600

    changes to excel

--- WEBSITE/excel_generate.php
+++ WEBSITE/excel_generate.php
@@ -1,9 +1,9 @@
 <?php
-include_once("connection.php");
+include_once("connect.php");
 $db = new dbObj();
 $connString =  $db->getConnstring();
 
-$sql_query = "SELECT * FROM patints";
+$sql_query = "SELECT * FROM Patient";
 $resultset = mysqli_query($connString, $sql_query) or die("database error:". mysqli_error($conn));
 $tasks = array();
 while( $rows = mysqli_fetch_assoc($resultset) ) {
