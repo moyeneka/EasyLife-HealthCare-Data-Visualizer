cc39616a6102c019da689077ecb7447a67689af9
scanales01 <scanal01@outlook.com>
2021-11-20 02:43:36 -0600

    Add Patient Page and CSS

---
+++ SQL/add_patient.py
@@ -0,0 +1,22 @@
+import sys
+import traceback
+import logging
+import python_db
+
+mysql_username = 'scanales' # please change to your username
+mysql_password= 'iepha3Oo'  # please change to your MySQL password
+
+try:
+    python_db.open_database('localhost', mysql_username, mysql_password, mysql_username)
+    FNAME = sys.argv[1]
+    LNAME = sys.argv[2]
+    SEX = sys.argv[3]
+    AGE = sys.argv[4]
+    ID = sys.argv[5]
+
+    val = "'" + FNAME + "','" + LNAME + "','" + SEX + "'," + AGE + "," + ID
+    python_db.insert("Patient", val)
+
+    python_db.close()
+except Exception as e:
+    logging.error(traceback.format_exc())
